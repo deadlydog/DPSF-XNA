2ea17d45d178c1485bde00f0eb422e1262df1337
deadlydog <deadlydog@hotmail.com>
2015-10-07 22:05:17 -0600

    - Added new DPSF FNA project to solution where DPSF is built using FNA (instead of XNA or MonoGame)

---
+++ XNA 4.0/DPSF/DPSF FNA/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("2d9bc0fe-cc76-4b6b-88e0-fcb919fba07d")]
